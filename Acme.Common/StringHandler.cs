788c039e948249df2a49fe80c858bece4022dfd0
zebby <zebbykhairah@gmail.com>
2019-03-10 18:32:29 +0800

    #8: Adding StringHandler class to Acme.Common project: #2

---
+++ Acme.Common/StringHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Common
+{
+    public class StringHandler
+    {
+        public string InsertSpace(string source)
+        {
+            string result = string.Empty;
+
+            if (!string.IsNullOrWhiteSpace(source))
+            {
+                for (int i=0; i<source.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        result += source[i];
+                        continue;
+                    }
+                    else
+                    {
+                        if (Char.IsUpper(source[i]))
+                        {
+                            result += " ";
+                        }
+                        result += source[i];
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
